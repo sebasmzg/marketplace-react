aa0ecd5d072d4918f993630847135c5c7ac3b385
sebasmzg <j.sebastianmzg@gmail.com>
2024-08-19 16:38:31 -0500

    render components and add title

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,18 @@
 import { Button, Container } from "@mui/material";
 import React from "react";
+import { HeaderComponent } from "../../components/header";
 
 export const HomePage: React.FC = () =>{
     
     return (
-        <Container sx={{mt:9}} maxWidth="xl">
-            <Button variant='contained'>Home</Button>
+        <Container  maxWidth="xl">
+            <HeaderComponent
+                title="Marketplace"
+                description="Welcome to the best marketplace"
+                element={
+                    <Button variant="contained" fullWidth sx={{padding: "0.5rem", fontSize: "1.5rem"}}>Start</Button>
+                }
+            />
         </Container>
     )
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/home";
-import { LoginPage } from "./pages/login/login";
+import { LoginPage } from "./pages/login";
 import { RouterLayout } from "./common/router-layout";
 
 export const AppRouter: React.FC = ()=>{
