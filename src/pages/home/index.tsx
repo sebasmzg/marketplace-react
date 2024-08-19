0c90a4197aec6d0423b17edd8294a98cdf1f3c63
sebasmzg <j.sebastianmzg@gmail.com>
2024-08-19 17:31:00 -0500

    get all characters

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
 import { Button, Container } from "@mui/material";
 import React from "react";
 import { HeaderComponent } from "../../components/header";
+import { characters } from "../../services/characters";
 
 export const HomePage: React.FC = () =>{
     
+    React.useEffect(()=>{
+        characters.getAll({page: 1}).then((res)=>{
+            console.log(res.data);            
+        }).catch((err)=>{{console.log(err)}});
+    },[]);
+
     return (
         <Container  maxWidth="xl">
             <HeaderComponent
---
+++ src/services/characters.ts
@@ -0,0 +1,9 @@
+import { instance } from "./base.api";
+
+const endPoint = "character";
+
+export const characters = {
+    getAll: function({page=1}:{page:number}){
+        return instance.get(endPoint,{params:{page}});
+    }
+}
