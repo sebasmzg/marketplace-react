bacdb7318a1c3c2a728290b8ef058d4fd2451b2e
sebasmzg <j.sebastianmzg@gmail.com>
2024-08-18 23:50:47 -0500

    login form and submit event

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages";
-import { LoginPage } from "./pages/home/login";
+import { HomePage } from "./pages/home";
+import { LoginPage } from "./pages/login/login";
 import { RouterLayout } from "./common/router-layout";
 
 export const AppRouter: React.FC = ()=>{
@@ -9,8 +9,8 @@ export const AppRouter: React.FC = ()=>{
         <Routes>
             <Route path="/" element={<RouterLayout/>}>
                 <Route path="/" element={<HomePage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
             </Route>
+            <Route path="/login" element={<LoginPage/>}/>
         </Routes>
     )
 }
